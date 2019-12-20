440b5cb73f13ca019ae2ccd3658297f75e1c4ef5
naveedsarwar <naveedrana@piri.ai>
2019-12-21 02:07:55 +0500

    Todo CRUD done

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -7,6 +7,7 @@ import { Link } from 'react-router-dom';
 function Home(props) {
     return (
         <div className="container">
+            <h1 style={{ textAlign: 'center', marginTop: 25, fontSize: 34 }}>Welcome to React Hooks</h1>
             <div style={{ width: '90%', display: 'flex', justifyContent: 'flex-end' }}>
                 <button style={{ marginLeft: '90%' }}>
                     <Link to="/add">Add Task</Link>
